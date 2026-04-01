4fa358e98be9882de9088a56736b61a9e72c1d1b
Dhara <dharaniviswanadh@example.com>
2026-04-01 22:15:08 +0530

    UC2: Fixed class and added command-line input

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-public class HelloWorld.java {
+public class HelloWorld {
     public static void main(String[] args) {
         if (args.length > 0) {
             System.out.println("Hello " + args[0]);
